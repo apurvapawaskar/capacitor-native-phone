aa75cb29a0bcbbf6dff8b95415b8dc1b912794ec
genora-apurva <100185438+genora-apurva@users.noreply.github.com>
2024-02-15 20:23:32 +0530

    Removed starter added function

--- android/src/main/java/com/apurvapawaskar/plugins/nativephone/NativePhone.java
+++ android/src/main/java/com/apurvapawaskar/plugins/nativephone/NativePhone.java
@@ -3,9 +3,4 @@ package com.apurvapawaskar.plugins.nativephone;
 import android.util.Log;
 
 public class NativePhone {
-
-    public String echo(String value) {
-        Log.i("Echo", value);
-        return value;
-    }
 }
